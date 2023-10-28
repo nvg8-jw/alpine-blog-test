a0b3f7e6b6f4364dd595d06730eadd55055c3916
nvg8-jw <149214002+nvg8-jw@users.noreply.github.com>
2023-10-28 12:08:25 -0700

    header

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'NVG8',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
@@ -9,7 +9,7 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -21,13 +21,13 @@ export default defineAppConfig({
         enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      instagram: 'nvg8.co',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
